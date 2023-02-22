7c2f89c5c16c7f8f1c1233f454e17994274a1122
ranpurakunj <ranpurakunj@gmail.com>
2023-02-22 05:59:43 -0500

    Created and rewrote controllers for Education_details table and user login table

---
+++ EntityFrameworkDbFirst/Controllers/Education_detailController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using EntityFrameworkDbFirst;
+
+namespace EntityFrameworkDbFirst.Controllers
+{
+    public class Education_detailController : Controller
+    {
+        private EmployeeDatabaseEntities1 db = new EmployeeDatabaseEntities1();
+
+        // GET: Education_detail
+        public ActionResult Index()
+        {
+            var education_detail = db.Education_detail.Include(e => e.Employee_master);
+            return View(education_detail.ToList());
+        }
+
+        // GET: Education_detail/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Education_detail education_detail = db.Education_detail.Find(id);
+            if (education_detail == null)
+            {
+                return HttpNotFound();
+            }
+            return View(education_detail);
+        }
+
+        // GET: Education_detail/Create
+        public ActionResult Create()
+        {
+            ViewBag.Employee_ID = new SelectList(db.Employee_master, "Employee_Id", "Employee_Name");
+            return View();
+        }
+
+        // POST: Education_detail/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Education_detail_id,Employee_ID,Education,University,Grade_Obtained,Passing_Year,Active_status")] Education_detail education_detail)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Education_detail.Add(education_detail);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Employee_ID = new SelectList(db.Employee_master, "Employee_Id", "Employee_Name", education_detail.Employee_ID);
+            return View(education_detail);
+        }
+
+        // GET: Education_detail/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Education_detail education_detail = db.Education_detail.Find(id);
+            if (education_detail == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Employee_ID = new SelectList(db.Employee_master, "Employee_Id", "Employee_Name", education_detail.Employee_ID);
+            return View(education_detail);
+        }
+
+        // POST: Education_detail/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Education_detail_id,Employee_ID,Education,University,Grade_Obtained,Passing_Year,Active_status")] Education_detail education_detail)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(education_detail).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Employee_ID = new SelectList(db.Employee_master, "Employee_Id", "Employee_Name", education_detail.Employee_ID);
+            return View(education_detail);
+        }
+
+        // GET: Education_detail/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Education_detail education_detail = db.Education_detail.Find(id);
+            if (education_detail == null)
+            {
+                return HttpNotFound();
+            }
+            return View(education_detail);
+        }
+
+        // POST: Education_detail/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Education_detail education_detail = db.Education_detail.Find(id);
+            db.Education_detail.Remove(education_detail);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ EntityFrameworkDbFirst/Controllers/tbl_User_TableController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using EntityFrameworkDbFirst;
+
+namespace EntityFrameworkDbFirst.Controllers
+{
+    public class tbl_User_TableController : Controller
+    {
+        private EmployeeDatabaseEntities1 db = new EmployeeDatabaseEntities1();
+
+        // GET: tbl_User_Table
+        public ActionResult Index()
+        {
+            return View(db.tbl_User_Table.ToList());
+        }
+
+        // GET: tbl_User_Table/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tbl_User_Table user_table = db.tbl_User_Table.Find(id);
+            if (user_table == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user_table);
+        }
+
+        // GET: tbl_User_Table/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: tbl_User_Table/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "User_Id,Username,Password,User_Full_Name,Last_Login_on,Active_status")] tbl_User_Table tbl_User_Table)
+        {
+            if (ModelState.IsValid)
+            {
+                db.tbl_User_Table.Add(tbl_User_Table);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(tbl_User_Table);
+        }
+
+        // GET: tbl_User_Table/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tbl_User_Table tbl_User_Table = db.tbl_User_Table.Find(id);
+            if (tbl_User_Table == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tbl_User_Table);
+        }
+
+        // POST: tbl_User_Table/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "User_Id,Username,Password,User_Full_Name,Last_Login_on,Active_status")] tbl_User_Table tbl_User_Table)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tbl_User_Table).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(tbl_User_Table);
+        }
+
+        // GET: tbl_User_Table/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tbl_User_Table tbl_User_Table = db.tbl_User_Table.Find(id);
+            if (tbl_User_Table == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tbl_User_Table);
+        }
+
+        // POST: tbl_User_Table/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            tbl_User_Table tbl_User_Table = db.tbl_User_Table.Find(id);
+            db.tbl_User_Table.Remove(tbl_User_Table);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
